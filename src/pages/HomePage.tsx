f58f0fff05f5fcf5b8e4c9c13917f3a07a4df37d
sakawat-sakib <mdsakawatsakib@gmail.com>
2024-12-10 22:23:58 +0600

    dhaka ctg

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,10 @@ import landingImage from "../assets/landing.png"
 import appDownloadImage from "../assets/appDownload.png"
 import SearchBar, { SearchForm } from "@/components/SearchBar"
 import {useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 const HomePage = () => {
+        const [location, setLocation] = useState("")
         const navigate = useNavigate()
         const handleSearchSubmit = (searchFromValues: SearchForm) => {
                 navigate({
@@ -15,7 +17,12 @@ const HomePage = () => {
             <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
                     <h1 className="text-5xl font-bold tracking-tight text-orange-600">Tuck into a takeway today</h1>
                     <span className="text-xl">Food is just a click away</span>
-                    <SearchBar placeHolder="Search City i.e Dhaka, Chattogram" onSubmit={handleSearchSubmit} />
+                    <SearchBar placeHolder="Search City i.e Dhaka, Chattogram" onSubmit={handleSearchSubmit} searchQuery={location}/>
+                    <div className="flex mx-auto gap-5">
+                        <span onClick={()=> setLocation("Dhaka")} className="px-3 py-1 bg-gray-100 rounded-full cursor-pointer hover:bg-gray-200">Dhaka</span>
+                        <span onClick={()=> setLocation("Chattogram")} className="px-3 py-1 bg-gray-100 rounded-full cursor-pointer hover:bg-gray-200">Chattogram</span>
+                        <span onClick={()=> setLocation("Manchester")} className="px-3 py-1 bg-gray-100 rounded-full cursor-pointer hover:bg-gray-200">Manchester</span>
+                    </div>
             </div>
 
             <div className="grid md:grid-cols-2 gap-5">
